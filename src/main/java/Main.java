e52c9faf81b5af8928454d99ea4913cac8218ee0
André do Nascimento <andredonascimentoferreira309@gmail.com>
2026-05-28 22:19:28 -0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import classes.Jogador;
+
+public class Main {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("André");
+
+        jogador.debug();
+    }
+}
---
+++ src/main/java/classes/Jogador.java
@@ -0,0 +1,64 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Jogador {
+    private UUID id;
+    private String nome;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+    private int pontuacao;
+    // vai ser posto a lista de partidas depois
+    ArrayList<Objects> historico;
+
+    public Jogador(String newName){
+        this.nome = newName;
+        this.id = UUID.randomUUID();
+        this.vitorias = 0;
+        this.empates = 0;
+        this.pontuacao = 200;
+        this.historico = new ArrayList<>();
+    }
+
+    public void registrarVitoria(){
+        this.vitorias++;
+        //não sei como funciona o esquema de pontuação
+        this.pontuacao += 100;
+    };
+
+    public void registrarDerrota(){
+        this.derrotas++;
+        //não sei como funciona o esquema de pontuação
+        if((this.pontuacao - 100) > 0){
+            this.pontuacao -= 100;
+        }else{
+            this.pontuacao = 0;
+        };
+
+    }
+
+    // METODO AINDA NÃO FEITO, PRECISA-SE DA CLASSE "PARTIDA PARA FUNCIOANR"
+    public void adicionarPartida(Objects partida){
+        this.historico.add(partida);
+    }
+
+    public double getTaxaVitoria(){
+        return  (double) (this.vitorias + this.derrotas + this.empates) / 10;
+    }
+
+    public void debug(){
+        System.out.println("Id:" + this.id);
+        System.out.println("Nome:" + this.nome);
+        System.out.println("Pontuação:" + this.pontuacao);
+        System.out.println("Vitórias:" + this.vitorias);
+        System.out.println("Derrotas:" + this.derrotas);
+        System.out.println("Empates:" + this.empates);
+
+        for(Object i : this.historico){
+            System.out.println("é para parecer aqui as partida");
+        }
+    }
+}
